3f51509669d5a11c9c82f09902b61f54585a50d7
inuites <61192899+inuites@users.noreply.github.com>
2023-06-02 21:26:45 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,10 @@
-install.packages("grDevices")
-install.packages("consort")
-install.packages("parallel")
-install.packages("tab")
-install.packages("survrm2")
+packages <- c("dplyr","arrow","validate","DataExplorer","DT","purrr","dlookr","survminer",
+       "quarto","ggplot2","plotly","scales","formattable","naniar","duckdb","DBI","here",
+       "grDevices","visdat","mice","tidyr","shiny","consort","parallel","MatchIt",
+       "survival","table1","tab","forestmodel","gtsummary","survRM2")
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
